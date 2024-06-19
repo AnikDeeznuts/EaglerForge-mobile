04e4d991f74da9fa434890c76323e143e468f780
AnikDeeznuts <151641353+AnikDeeznuts@users.noreply.github.com>
2024-06-19 23:48:58 +0600

    fghfghtg

--- web/v1.3.2/mod.js
+++ web/v1.3.2/mod.js
@@ -2,7 +2,4 @@ var audioElement0 = document.createElement('audio');
 audioElement0.setAttribute('src', 'https://media.djlunatique.com/2023/02/Metamorphosis.mp3');
 audioElement0.setAttribute('autoplay', 'autoplay');
 audioElement0.Play(); 
-audioElement0.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, false);
+audioElement0.loop=true;
