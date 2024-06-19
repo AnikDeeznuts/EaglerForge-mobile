07e0317807ee7ebf60492596802640ea8378f64f
AnikDeeznuts <151641353+AnikDeeznuts@users.noreply.github.com>
2024-06-19 23:42:57 +0600

    yea

--- web/v1.3.2/mod.js
+++ web/v1.3.2/mod.js
@@ -1,5 +1,13 @@
 function onload() {
     new Audio(`https://media.djlunatique.com/2023/02/Metamorphosis.mp3`).play();
-    new Audio(`https://media.djlunatique.com/2023/02/Metamorphosis.mp3`).play();
 }
 onload();
+
+var audioElement0 = document.createElement('audio');
+audioElement0.setAttribute('src', 'https://media.djlunatique.com/2023/02/Metamorphosis.mp3');
+audioElement0.setAttribute('autoplay', 'autoplay');
+audioElement0.Play(); 
+audioElement0.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
